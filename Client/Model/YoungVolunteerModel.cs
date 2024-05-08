955768f9c264d3d954368503576c616db5720106
SakaEaaa <146702440+SakaEaaa@users.noreply.github.com>
2024-05-08 13:15:02 +0200

    administrator feature

---
+++ Client/Model/YoungVolunteerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Model
+{
+    public class YoungVolunteerModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Signature { get; set; }
+    }
+}
